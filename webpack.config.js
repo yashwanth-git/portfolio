35a2e9abf5882857ec60c6ec0c28a355f2639bbf
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-04-11 21:37:05 -0400

    minify setup for html, css, js

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// Webpack uses this to work with directories
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: "./src/main.js",
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "",
+    filename: "../dist/main.min.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "../dist/index.html",
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        minifyCSS: true,
+      },
+    }),
+  ],
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript
+  // minifying and other things, so let's set mode to development
+  mode: "development",
+};
